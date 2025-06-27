801097a1e9dc3c44ed69c955f0fa71064aac02b4
Kavya <kavyakopatti99@gmail.com>
2025-06-27 13:57:55 +0530

    signup and signin

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Login from '../src/components/login' 
+import{ useState} from 'react'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+
+const App=()=>{
+  
+  const appRouter=createBrowserRouter([
+    {
+      path:'/',
+      element:<Login/>
+    }
+  ])
+
+  
+  return(
+   <div>
+   <RouterProvider router={appRouter}/>
+   </div>
+
+  )
+}
+
+export default App;
---
+++ src/components/login.jsx
@@ -0,0 +1,83 @@
+ import Header from "./header";
+ import {useState} from 'react'
+
+const Login=()=>{
+  const [isSignInForm,setIsSignInForm]=useState(true)
+
+  const toggleSignInForm=()=>{
+    setIsSignInForm(!isSignInForm)
+  }
+return(
+  <div>
+    <Header/>
+    <div className="absolute">
+      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_large.jpg" alt="" />
+    </div>
+   
+    <form className="absolute w-4/12 bg-black/70 p-12 my-40 mx-auto right-0 left-0 rounded text-white">
+    <h1 className="text-3xl font-bold mb-4">{isSignInForm?"Sign In":"Sign Up"}</h1>
+
+
+    <div className="relative mb-6">
+   <input
+    type="text"
+    id="email"
+    placeholder=" "
+    required
+    className="peer h-12 w-full px-4 pt-6 pb-2 bg-white/10 text-white rounded border border-white/20 placeholder-transparent focus:outline-none focus:border-white focus:bg-white/20 transition-all"
+  />
+  <label
+    htmlFor="email"
+    className="absolute left-4 top-2 text-xs text-gray-400 transition-all 
+      peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-base
+      peer-focus:top-1 peer-focus:text-sm peer-focus:text-white"
+  >
+    Email or phone number
+  </label>
+</div>
+
+
+<div className="relative mb-6">
+  <input
+    type="password"
+    id="password"
+    placeholder=" "
+    required
+    className="peer h-12 w-full px-3 pt-5 pb-2 bg-white/10 text-white rounded border border-white/20 placeholder-transparent focus:outline-none focus:border-white focus:bg-white/20 transition-all"
+  />
+  <label
+    htmlFor="password"
+    className="absolute left-3 top-2 text-sm text-gray-400 transition-all 
+      peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-base
+      peer-focus:top-2 peer-focus:text-sm peer-focus:text-white"
+  >
+    Password
+  </label>
+</div>
+ 
+  <button className="w-full my-4 p-2 rounded bg-red-600">{isSignInForm?"Sign In":"Sign Up"}</button>
+  {isSignInForm && (
+    <>
+  <p className="text-center">OR</p>
+  <button className="w-full my-4 p-2 rounded bg-gray-700">Use Code</button>
+
+  <p className="text-center text-sm text-gray-400 cursor-pointer hover:underline mb-4">Forgot Password?</p>
+
+  <div className="flex items-center gap-2 text-sm text-gray-300 mb-2">
+    <input type="checkbox" id="remember" />
+    <label htmlFor="remember">Remember me</label>
+   
+    </div>
+     </>
+   )}
+
+  <p className="text-sm text-gray-400">{isSignInForm?"New to Netflix?":"Already have Account?"}
+<span className="text-white hover:underline cursor-pointer" onClick={toggleSignInForm}>{isSignInForm?"Sign up now":" Sign In now"}</span>
+  </p>
+</form>
+
+  </div>
+)
+}
+
+export default Login;
